53096e36eb145b5bf857a47f467f446b06f8e9a0
milos970 <dev980lop@gmail.com>
2022-11-04 15:10:57 +0100

    Nazdar kuci

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Main {
+    /**
+     * Constructor for objects of class Main
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello fellas");
+    }
+}
