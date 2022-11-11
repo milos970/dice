b39335d1c944821404642f100adb22f947561f61
milos970 <dev980lop@gmail.com>
2022-11-11 14:49:37 +0100

    Info klasy

--- Main.java
+++ Main.java
@@ -10,13 +10,9 @@ public class Main {
      * Constructor for objects of class Main
      */
     public static void main(String[] args) {
-        System.out.println(vasaFantazia());
+        Platno.dajPlatno();
     }
     
-    /*
-     * Fantazii sa medze nekladu
-     */
-    public static String vasaFantazia() {
-        return null;
+    
 }
-}
+
