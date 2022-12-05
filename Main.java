9dc39e4908c7c522035c1e5984236d978af3a2d8
milos970 <dev980lop@gmail.com>
2022-12-05 13:28:54 +0100

    Čistý štít.

---
+++ Main.java
@@ -0,0 +1,14 @@
+import com.formdev.flatlaf.FlatLightLaf;
+public class Main {
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+        
+        
+        System.out.print('\u000C'); //vyčisti konzolu
+
+          
+        
+        
+    }
+}
