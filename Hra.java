c0033517268f595141293e7c2077a5827a5e9b66
Lukáš Petrek <blackburn1805@gmail.com>
2022-12-10 00:04:44 +0100

    Pridanie metody actionPerformed do Hra

--- Hra.java
+++ Hra.java
@@ -35,6 +35,57 @@ public class Hra {
         this.pouzivatelomZastavene = false;
     }
     
+        /**
+     * Vytvorenie vlákna, zobrazenie grafu, kociek podľa zvoleného počtu
+     * Spustenie a zastavenie hry cez tlacitko
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+
+        if (this.vytvorene == false) {
+            this.vytvorene = true;
+            this.thread = null;
+            this.thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        spust();
+                    }
+                }); 
+        }
+
+        //pokial hra nebezi a je kliknuté na RadioButton
+        //osetruje prípad, kedy je kliknuté na rovnaký RadioButton
+        if (bezi == false && (e.getSource() instanceof JRadioButton)) {
+            if (this.zvolena == this.nastaveniePoctu.zvolena()) {
+                return;
+            }
+            this.zvolena = this.nastaveniePoctu.zvolena();
+            this.nastavenieKociek.zobrazKocky(this.nastaveniePoctu.zvolena());
+            this.hodKockami.zobrazKocky(this.nastaveniePoctu.zvolena());
+            
+            //podla zvoleneho poctu kociek vytvori prazdny graf
+            this.histogram.nastavPocetKociek(this.nastaveniePoctu.zvolena());
+            this.histogram.vykresli();
+            //
+            return;
+        }
+
+        //spustí hru
+        if (bezi == false) {
+            this.pocitadlo.vynuluj();
+            this.nastavenieKociek.aktivuj(false);
+            this.nastaveniePoctu.aktivuj(false);
+            this.bezi = true;
+            this.pouzivatelomZastavene = false;
+            this.tlacitko.aktivuj();
+            this.thread.start();
+        } else {
+            //zastaví hru
+            this.pouzivatelomZastavene = true;
+        }
+    }
+    
     private void spust() 
     {
         int sucetHodov = 0;
