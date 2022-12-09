9bcf5add4f885f6453ebfba5c907e29cc0dcdcc1
milos970 <dev980lop@gmail.com>
2022-12-09 22:09:53 +0100

    Pridanie tried Main a Menu.

---
+++ Main.java
@@ -0,0 +1,15 @@
+import com.formdev.flatlaf.FlatLightLaf;
+public class Main {
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+        
+        
+        
+        System.out.print('\u000C'); //vyčisti konzolu
+
+          
+        
+        
+    }
+}
